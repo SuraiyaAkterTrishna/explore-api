517248063cc130409198659799b7606ec9094a82
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-02 16:47:14 +0600

    display user name on the UI

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ function loadUsers2(){
     .then(data => displayUser(data))
 }
 function displayUser(data){
-    console.log(data);
+    const ul = document.getElementById('user-list');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
+    }
 };
