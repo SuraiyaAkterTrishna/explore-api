f19684012c96b180de586ceaaee8bc17946e1c3a
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-02 16:32:30 +0600

    index.js fetch data

---
+++ index.js
@@ -0,0 +1,9 @@
+function loadUsers2(){
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayUser(data))
+}
+function displayUser(data){
+    console.log(data);
+};
