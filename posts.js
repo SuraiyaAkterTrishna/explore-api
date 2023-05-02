aa3cbf92cdef4e4caf233e1c6c038f77a9551bb9
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-02 22:39:17 +0600

    load post done

--- posts.js
+++ posts.js
@@ -1,1 +1,13 @@
-console.log("posts loaded");
+function loadPosts(){
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+
+function displayPosts(posts){
+    for (const post of posts) {
+        console.log(post);
+    }
+}
+loadPosts();
