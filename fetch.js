9db6b3e0387e4e55fecf704924a80d6b5deeb57a
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-02 16:17:39 +0600

    fetch users data

--- fetch.js
+++ fetch.js
@@ -3,3 +3,11 @@ function loadData2(){
     .then(res => res.json())
     .then(data => console.log(data))
 }
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUser(data))
+}
+function displayUser(data){
+    console.log(data);
+}
