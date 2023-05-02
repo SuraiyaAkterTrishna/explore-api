2d162b58f4749dd7158850270533c3378ee6a7f2
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-02 23:40:30 +0600

    work complete for today

--- posts.js
+++ posts.js
@@ -9,11 +9,13 @@ function displayPosts(posts){
     const postContainer = document.getElementById('post-container');
     for (const post of posts) {
         const div = document.createElement('div');
+        div.classList.add('post');
         div.innerHTML = `
-            <h4>User - </h4>
-            <h5>Post - title </h5>
-            <p>Description: </p>
-        `
+            <h4>User - ${post.userId}</h4>
+            <h5>Post - ${post.title} </h5>
+            <p>Description: ${post.body}</p>
+        `;
+        postContainer.appendChild(div);
     }
 }
 loadPosts();
